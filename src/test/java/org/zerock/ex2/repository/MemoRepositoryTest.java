f3c838be82467e73b8529f602f37532092cc2c4c
bominjang <qhdud9443@naver.com>
2021-07-31 23:59:41 +0900

    test : 페이징 처리
    - findAll에 인자로 Pageable 타입을 전달하면 return은 Page<엔티티 타입>으로 됨.
    - 쿼리 결과를 사용하기 위해 여러 메소드 사용 가능.

--- src/test/java/org/zerock/ex2/repository/MemoRepositoryTest.java
+++ src/test/java/org/zerock/ex2/repository/MemoRepositoryTest.java
@@ -3,6 +3,9 @@ package org.zerock.ex2.repository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
 import org.zerock.ex2.entity.Memo;
 
@@ -73,6 +76,17 @@ class MemoRepositoryTest {
         Long mno = 100L;
 
         memoRepository.deleteById(mno);
+    }
+
+    @Test
+    public void testPageDefault(){
+        //1페이지 10개
+        Pageable pageable = PageRequest.of(0,10);
+
+        Page<Memo> result  = memoRepository.findAll(pageable);
+
+        System.out.println(result);
+
     }
 
 }
